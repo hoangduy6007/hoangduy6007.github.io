21b9d4431febc5e244379e4ac9f48887c16f357e
hoangduy6007 <46916957+hoangduy6007@users.noreply.github.com>
2019-01-27 19:18:19 +0700

    upo

--- main.js
+++ main.js
@@ -37,9 +37,7 @@ function playStream(idVideoTag, stream) {
 //openStream()
 //.then(stream => playStream('localStream', stream));
 
-const peer = new Peer({
-    key: 'peerjs',
-});
+var peer = new Peer({ key: 'peerjs', host: "skipye.herokuapp.com", secure: true, port: 443 });
 
 peer.on('open', id => {
     $('#my-peer').append(id);
