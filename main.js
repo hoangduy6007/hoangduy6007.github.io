98f1f26805374325a7eacc13883f7e5931f076f7
hoangduy6007 <46916957+hoangduy6007@users.noreply.github.com>
2019-01-27 12:05:22 +0700

    last up

--- main.js
+++ main.js
@@ -2,27 +2,47 @@ const socket = io('https://streamdemo1.herokuapp.com/');
 
 $('#div-chat').hide();
 
+let customConfig;
+
+$.ajax({
+  url: "https://service.xirsys.com/ice",
+  data: {
+    ident: "vanpho",
+    secret: "2b1c2dfe-4374-11e7-bd72-5a790223a9ce",
+    domain: "hoangduy6007.github.io",
+    application: "default",
+    room: "default",
+    secure: 1
+  },
+  success: function (data, status) {
+    // data.d is where the iceServers object lives
+    customConfig = data.d;
+    console.log(customConfig);
+  },
+  async: false
+});
+
 socket.on('DANH_SACH_ONLINE', arrUserInfo => {
+    $('#div-chat').show();
+    $('#div-dang-ky').hide();
 
-  $('#div-chat').show();
-  $('#div-dang-ky').hide();
+    arrUserInfo.forEach(user => {
+        const { ten, peerId } = user;
+        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
+    });
 
-  arrUserInfo.forEach(user => {
-      const { ten, peerId } = user;
-      $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
-  });
+    socket.on('CO_NGUOI_DUNG_MOI', user => {
+        const { ten, peerId } = user;
+        $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
+    });
 
-  socket.on('CO_NGUOI_DUNG_MOI', user => {
-    const { ten, peerId } = user;
-    $('#ulUser').append(`<li id="${peerId}">${ten}</li>`);
-  });
-
-  socket.on('AI_DO_NGAT_KET_NOI', peerId => {
-      $(`#${peerId}`).remove();
-  });
-})
+    socket.on('AI_DO_NGAT_KET_NOI', peerId => {
+        $(`#${peerId}`).remove();
+    });
+});
 
 socket.on('DANG_KY_THAT_BAT', () => alert('Vui long chon username khac!'));
+
 
 function openStream() {
     const config = { audio: false, video: true };
@@ -34,10 +54,17 @@ function playStream(idVideoTag, stream) {
     video.srcObject = stream;
     video.play();
 }
-//openStream()
-//.then(stream => playStream('localStream', stream));
 
-var peer = new Peer({ key: 'peerjs', host: "https://streamdem.herokuapp.com/", secure: true, port: 3000 });
+// openStream()
+// .then(stream => playStream('localStream', stream));
+
+const peer = new Peer({
+    key: 'peerjs',
+    host: 'https://streamdem.herokuapp.com/',
+    secure: true,
+    port: 443,
+    config: customConfig
+});
 
 peer.on('open', id => {
     $('#my-peer').append(id);
@@ -47,6 +74,7 @@ peer.on('open', id => {
     });
 });
 
+//Caller
 $('#btnCall').click(() => {
     const id = $('#remoteId').val();
     openStream()
@@ -56,6 +84,8 @@ $('#btnCall').click(() => {
         call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
     });
 });
+
+//Callee
 peer.on('call', call => {
     openStream()
     .then(stream => {
