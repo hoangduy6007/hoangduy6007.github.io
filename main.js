214ff88e209128c8b309dc60a4eeac630bf873f6
hoangduy6007 <46916957+hoangduy6007@users.noreply.github.com>
2019-02-18 22:58:07 +0700

    Update main.js
    up 2

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ function openStream() {
 
 function playStream(idVideoTag, stream) {
     const video = document.getElementById(idVideoTag);
+    document.getElementById(‘video’).src = window.URL.createObjectURL(stream);
     video.srcObject = stream;
     video.play();
 }
